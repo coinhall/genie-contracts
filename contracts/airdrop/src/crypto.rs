48144cd83c49c075d5428a82137464c1e956321a
Aaron Choo <choozy.aaron@gmail.com>
2023-01-06 15:07:34 +0800

    refactor: improve code quality

--- contracts/airdrop/src/crypto.rs
+++ contracts/airdrop/src/crypto.rs
@@ -1,8 +1,7 @@
-use cosmwasm_std::{Addr, DepsMut, StdError, StdResult, Uint128};
-use hex::encode;
+use cosmwasm_std::{Addr, DepsMut, StdResult, Uint128};
 use sha3::{Digest, Keccak256};
 
-pub fn verify_claim(
+pub fn is_valid_signature(
     deps: &DepsMut,
     account: &Addr,
     asset_string: &String,
@@ -14,16 +13,9 @@ pub fn verify_claim(
     let msg_buf = msg.as_bytes();
     let keccak_digest = Keccak256::digest(msg_buf);
     let hash = keccak_digest.as_slice();
-
     let result = deps.api.secp256k1_verify(&hash, &signature, &public_key);
-
     match result {
         Ok(true) => Ok(true),
-        Ok(false) => Err(StdError::generic_err(format!(
-            "Claim failed! Signature verification failed. Hash:{}, Msg:{}",
-            encode(hash),
-            msg,
-        ))),
-        Err(err) => Err(err.into()),
+        _ => Ok(false),
     }
 }
